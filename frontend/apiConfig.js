a1aab16bb684ba41c08d86111b936ac396ea3680
DavidNana <davidnana.dwomohsarpong@gmail.com>
2025-07-25 16:48:09 -0400

    Update API URL for production and centralize config

---
+++ frontend/apiConfig.js
@@ -0,0 +1,5 @@
+// This file centralizes the API URL for the entire application.
+// If you ever change your backend hosting, you only need to update it here.
+
+// IMPORTANT: Replace this with the public URL of your deployed backend on Render.
+export const API_URL = 'https://last-checked-in-api.onrender.com/api'; 
